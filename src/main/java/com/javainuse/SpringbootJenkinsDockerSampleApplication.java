f980c42e355ab8ab429f3093b5d7d4745ea1d551
vbraos <53826544+vbraos@users.noreply.github.com>
2020-04-24 15:30:55 +0530

    first commit

---
+++ src/main/java/com/javainuse/SpringbootJenkinsDockerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDockerSampleApplication.class, args);
+	}
+
+}
